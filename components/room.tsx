d218e6654b422f4a864b28d899360eb2d3efa2e5
ryuji <yrejgnl11@163.com>
2024-03-08 20:38:31 +0800

    feat: insert layer

--- components/room.tsx
+++ components/room.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { RoomProvider } from "@/liveblocks.config";
+import { Layer } from "@/types/canvas";
+import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
 import { ClientSideSuspense } from "@liveblocks/react";
 
 interface RoomProps {
@@ -9,12 +11,19 @@ interface RoomProps {
   fallback: NonNullable<React.ReactNode> | null;
 }
 
-export const Room = ({children, roomId, fallback}: RoomProps) => {
+export const Room = ({ children, roomId, fallback }: RoomProps) => {
   return (
-    <RoomProvider id={roomId} initialPresence={{cursor: null}}>
+    <RoomProvider
+      id={roomId}
+      initialPresence={{ cursor: null, selection: [] }}
+      initialStorage={{
+        layers: new LiveMap<string, LiveObject<Layer>>(),
+        layerIds: new LiveList(),
+      }}
+    >
       <ClientSideSuspense fallback={fallback}>
         {() => children}
       </ClientSideSuspense>
     </RoomProvider>
-  )
-}
+  );
+};
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,5 +1,11 @@
-import { createClient } from "@liveblocks/client";
+import {
+  LiveList,
+  LiveMap,
+  LiveObject,
+  createClient,
+} from "@liveblocks/client";
 import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
+import { Layer } from "./types/canvas";
 
 const client = createClient({
   throttle: 16,
@@ -11,6 +17,7 @@ const client = createClient({
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
   cursor: { x: number; y: number } | null;
+  selection: string[];
   // ...
 };
 
@@ -19,8 +26,8 @@ type Presence = {
 // LiveList, LiveMap, LiveObject instances, for which updates are
 // automatically persisted and synced to all connected clients.
 type Storage = {
-  // author: LiveObject<{ firstName: string, lastName: string }>,
-  // ...
+  layers: LiveMap<string, LiveObject<Layer>>;
+  layerIds: LiveList<string>;
 };
 
 // Optionally, UserMeta represents static/readonly metadata on each user, as
