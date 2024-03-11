34123b7aae365565a938c9fd07c0c489256e8ab7
ryuji <yrejgnl11@163.com>
2024-03-11 21:06:24 +0800

    feat: layer selection tool

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -31,6 +31,7 @@ import {
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 
@@ -294,6 +295,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         undo={history.undo}
         redo={history.redo}
       />
+      <SelectionTools camera={camera} setLastUsedColor={setLastUsedColor}/>
       <svg
         className="h-[100vh] w-[100vw]"
         onWheel={onWheel}
--- lib/utils.ts
+++ lib/utils.ts
@@ -25,7 +25,7 @@ export function pointerEventToCanvasPoint(
 export function colorToCss(color: Color) {
   return `#${color.r.toString(16).padStart(2, "0")}${color.g
     .toString(16)
-    .padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}}`;
+    .padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
 }
 
 export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
